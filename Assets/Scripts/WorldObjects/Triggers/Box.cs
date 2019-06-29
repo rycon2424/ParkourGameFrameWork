4614d2cd5165ab849084bc5e04dc8fb6ec06d029
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2019-06-29 12:16:39 +0200

    When pusing boxes now cant go through walls

--- Assets/Scripts/WorldObjects/Triggers/Box.cs
+++ Assets/Scripts/WorldObjects/Triggers/Box.cs
@@ -12,7 +12,6 @@ public class Box : MonoBehaviour
 
     public bool playerLocked = false;
     public float playerOffset;
-    public float distance;
 
     void Start()
     {
@@ -26,7 +25,7 @@ public class Box : MonoBehaviour
     {
         if (playerLocked)
         {
-            anim.SetFloat("Push Speed", Input.GetAxis("Vertical"));
+            CheckingRaycast();
         }
         if (Input.GetKeyDown(pi.action) && playerLocked)
         {
@@ -37,42 +36,68 @@ public class Box : MonoBehaviour
             playerLocked = false;
         }
     }
-    
-    RaycastHit hit;
 
+    RaycastHit hitForward;
+    public float distanceForward;
+    RaycastHit hitBack;
+    public float distanceBack;
+
+    void CheckingRaycast()
+    {
+        anim.SetFloat("Push Speed", Input.GetAxis("Vertical"));
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.DrawRay(new Vector3(player.transform.position.x, player.transform.position.y + 0.3f, player.transform.position.z), player.transform.forward * distanceForward, Color.blue);
+            if (Physics.Raycast(new Vector3(player.transform.position.x, player.transform.position.y + 0.3f, player.transform.position.z), player.transform.forward, out hitForward, distanceForward))
+            {
+                if (hitForward.collider.tag != "")
+                {
+                    Debug.Log("Blocked");
+                    anim.applyRootMotion = false;
+                    return;
+                }
+            }
+            anim.applyRootMotion = true;
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.DrawRay(new Vector3(player.transform.position.x, player.transform.position.y + 0.3f, player.transform.position.z), -player.transform.forward * distanceBack, Color.magenta);
+            if (Physics.Raycast(new Vector3(player.transform.position.x, player.transform.position.y + 0.3f, player.transform.position.z), -player.transform.forward, out hitBack, distanceBack))
+            {
+                if (hitBack.collider.tag != "")
+                {
+                    Debug.Log("Blocked");
+                    anim.applyRootMotion = false;
+                    return;
+                }
+            }
+            anim.applyRootMotion = true;
+        }
+    }
+    
     void OnTriggerStay(Collider col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
             if (Input.GetKeyDown(pi.action) && !playerLocked && anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
             {
-                /*if (Physics.Raycast(player.transform.position, (transform.position - player.transform.position).normalized * distance, out hit))
-                {
-                    Debug.DrawRay(player.transform.position, (transform.position - player.transform.position).normalized * distance, Color.yellow);
-                    if (hit.collider.CompareTag("Box"))
-                    {*/
-                        Debug.Log("EnterBox");
-                        pc.RotateToTarget(transform.position);
-                        //Debug.Log(hit.normal);
-                        //pc.RotateToTarget(hit.normal);
-                        transform.parent = GameObject.FindObjectOfType<PlayerController>().transform;
-                        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, 0.23f);
-                        anim.SetFloat("Speed", 0);
-                        anim.SetBool("PushBox", true);
-                        pc.enabled = false;
-                        Invoke("LockPlayer", 0.5f);
-                    //}
-                //}
+                Debug.Log("EnterBox");
+                pc.RotateToTarget(transform.position);
+                transform.parent = GameObject.FindObjectOfType<PlayerController>().transform;
+                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, 0.23f);
+                anim.SetFloat("Speed", 0);
+                anim.SetBool("PushBox", true);
+                pc.enabled = false;
+                Invoke("LockPlayer", 0.5f);
             }
         }
     }
 
     void OnTriggerExit(Collider col)
     {
-        if (col.gameObject.CompareTag("Player"))
-        {
 
-        }
     }
 
     void LockPlayer()
