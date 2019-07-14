a1b8d726a258d087347a40d1cd22889183db80a7
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2019-07-14 20:20:12 +0200

    Cutscene 70% done

---
+++ Assets/Scripts/Cutscenes/Intro Cutscene/IntroCutscene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroCutscene : MonoBehaviour
+{
+
+    [Header("Character animations")]
+    public Animator anim;
+    public GameObject hud;
+    public GameObject maincam;
+
+    public void DisableHUD()
+    {
+        hud.SetActive(false);
+        maincam.SetActive(false);
+    }
+
+    public void Walk()
+    {
+        anim.SetBool("Look", false);
+    }
+
+    public void LookAround()
+    {
+        anim.SetBool("Look", true);
+    }
+
+    public void Die()
+    {
+        anim.SetBool("Look", false);
+    }
+}
