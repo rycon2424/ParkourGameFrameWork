16dba1004b87a2978694f5f867cd4b2808597aeb
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2019-07-14 18:21:02 +0200

    final preps till making end

--- Assets/Scripts/WorldObjects/Triggers/HurtZone.cs
+++ Assets/Scripts/WorldObjects/Triggers/HurtZone.cs
@@ -8,12 +8,17 @@ public class HurtZone : MonoBehaviour
     public int amount = 1;
 
     private PlayerStats stats;
+    private HorseMovement hm;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             stats = other.gameObject.GetComponent<PlayerStats>();
+        }
+        if (other.CompareTag("Horse"))
+        {
+            hm = other.gameObject.GetComponentInParent<HorseMovement>();
         }
     }
 
@@ -22,6 +27,10 @@ public class HurtZone : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             stats.Health -= amount;
+        }
+        if (other.CompareTag("Horse"))
+        {
+            hm.health -= 50;
         }
     }
 
