891d9c4c23d7d06a475556d73723142f692b6d10
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2019-07-16 13:32:04 +0200

    Paar bugs fixed

---
+++ Assets/Scripts/UI/ToggleOnRange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleOnRange : MonoBehaviour
+{
+    public float range;
+    public Transform target;
+    public GameObject goToShow;
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target.position) < range)
+        {
+            goToShow.SetActive(true);
+        }
+        else
+        {
+            goToShow.SetActive(false);
+        }
+    }
+
+}
