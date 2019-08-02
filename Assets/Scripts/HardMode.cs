0f2c653e91908ab2d01fc45e115e235936daefc0
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2019-08-02 23:25:36 +0200

    First patch

---
+++ Assets/Scripts/HardMode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HardMode : MonoBehaviour
+{
+    
+    public GameObject guard;
+    public bool hardmode = false;
+    
+    void Update()
+    {
+        if (hardmode == false && Input.GetKeyDown(KeyCode.K))
+        {
+            guard.SetActive(true);
+            hardmode = true;
+        }
+    }
+}
